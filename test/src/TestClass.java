164e3d7912ef7ef20c9a5a3a8b6fc0a0f6ddb6b8
unknown <SBT-Belokozov-AV@SBT-NSK-IK0014.sigma.sbrf.ru>
2016-04-14 15:04:34 +0600

    Initial commit

---
+++ test/src/TestClass.java
@@ -0,0 +1,10 @@
+/**
+ * Created by SBT-Belokozov-AV on 12.04.2016.
+ */
+
+public class TestClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Idea!");
+    }
+}
