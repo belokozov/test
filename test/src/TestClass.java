8a6deb0ba2b67e180feabb54aea6cef52c70b330
belokozov <belokozov@gmail.com>
2016-04-19 13:59:03 +0600

    util main

--- test/src/TestClass.java
+++ test/src/TestClass.java
@@ -4,6 +4,7 @@
 
 public class TestClass {
 
+
     public static void main(String[] args) {
         System.out.println("Hello, Idea!");
         System.exit(0);
--- test/src/Util.java
+++ test/src/Util.java
@@ -2,4 +2,7 @@
  * Created by SBT-Belokozov-AV on 19.04.2016.
  */
 public class Util {
+    private static final id = 0;
+
+    public static void main(String[] args){}
 }
